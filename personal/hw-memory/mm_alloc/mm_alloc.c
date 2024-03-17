797f42184ad06a82f193b1ea201068a7e7f3577b
is-zq <1056180862@qq.com>
2024-03-17 20:08:45 +0800

    hw-memory:mm_alloc

--- personal/hw-memory/mm_alloc/mm_alloc.c
+++ personal/hw-memory/mm_alloc/mm_alloc.c
@@ -9,18 +9,132 @@
 #include <stdio.h>
 #include <string.h>
 
-void* mm_malloc(size_t size) {
-  //TODO: Implement malloc
+#define true 1
+#define false 0
 
-  return NULL;
+typedef struct MetaData
+{
+	size_t size;
+	int free;
+	struct MetaData* next;
+	struct MetaData* prev;
+}MetaData,*MemList;
+
+MemList mem_list = NULL;
+
+/* Insert MetaData Node of size SIZE after P */
+MetaData* metadata_insert(MetaData* p,size_t size,int free)
+{
+	char* q = (char*)p;
+	q += sizeof(MetaData) + p->size;
+	MetaData* s = (MetaData*)q;
+	s->next = p->next;
+	s->prev = p;
+	p->next = s;
+	if(s->next != NULL)
+		s->next->prev = s;
+	
+	s->size = size;
+	s->free = free;
+	return s;
+}
+
+void* mm_malloc(size_t size) {
+	if(size == 0)
+		return NULL;
+
+	if(mem_list == NULL)	//Empty memory list
+	{
+		void* ret_p = sbrk(sizeof(MetaData) + size);
+		if(ret_p == (void*)-1)
+			return NULL;
+		mem_list = (MemList)ret_p;
+		mem_list->next = NULL;
+		mem_list->prev = NULL;
+		mem_list->size = size;
+		mem_list->free = false;
+		memset(mem_list+1,0,size);
+		return mem_list + 1;
+	}
+
+	MetaData* p = mem_list;
+	MetaData* tail = p;
+	while(p != NULL)
+	{
+		if(p->free && p->size >= size)
+		{
+			p->free = false;
+			if(p->size - size > sizeof(MetaData))	//Sufficient remaining space
+			{
+				size_t remain_size = p->size - size - sizeof(MetaData);
+				p->size = size;
+				metadata_insert(p,remain_size,true);
+			}
+			memset(p+1,0,p->size);
+			return p + 1;
+		}
+		else
+		{
+			tail = p;
+			p = p->next;
+		}
+	}
+
+	/* No sufficient block */
+	if(sbrk(sizeof(MetaData) + size) == (void*)-1)
+		return NULL;
+	MetaData* ret_p = metadata_insert(tail,size,false);
+	memset(ret_p+1,0,size);
+	return ret_p + 1;
 }
 
 void* mm_realloc(void* ptr, size_t size) {
-  //TODO: Implement realloc
+	if(ptr != NULL && size == 0)
+	{
+		mm_free(ptr);
+		return NULL;
+	}
+	else if(ptr == NULL)
+		return mm_malloc(size);
 
-  return NULL;
+	void* new_p = mm_malloc(size);
+	MetaData* metadata = (MetaData*)ptr;
+	metadata--;
+	memcpy(new_p,ptr,metadata->size);
+	mm_free(ptr);
+
+	return new_p;
 }
 
 void mm_free(void* ptr) {
-  //TODO: Implement free
+	if(mem_list == NULL || ptr == NULL)
+		return;
+
+	MetaData* p = mem_list;
+	while(p != NULL)
+	{
+		if(p+1 == ptr)
+		{
+			p->free = true;
+			MetaData* pre_p = p->prev;
+			MetaData* next_p = p->next;
+			if(next_p != NULL && next_p->free == true)
+			{
+				p->size += sizeof(MetaData) + next_p->size;
+				p->next = next_p->next;
+				if(next_p->next != NULL)
+					next_p->next->prev = p;
+			}
+			if(pre_p != NULL && pre_p->free == true)
+			{
+				pre_p->size += sizeof(MetaData) + p->size;
+				pre_p->next = p->next;
+				if(p->next != NULL)
+					p->next->prev = pre_p;
+			}
+			break;
+		}
+		else
+			p = p->next;
+	}
 }
--- personal/hw-memory/mm_alloc/mm_test.c
+++ personal/hw-memory/mm_alloc/mm_test.c
@@ -33,9 +33,59 @@ static void load_alloc_functions() {
 int main() {
   load_alloc_functions();
 
-  int* data = mm_malloc(sizeof(int));
-  assert(data != NULL);
-  data[0] = 0x162;
-  mm_free(data);
-  puts("malloc test successful!");
+  long long* p1 = mm_malloc(sizeof(long long));
+  int* p2 = mm_malloc(sizeof(int));
+
+  char* str = mm_malloc(sizeof(char)*12);
+  str[0] = 'a',str[1] = 'b',str[2] = 'c';
+
+  int* arr = mm_malloc(sizeof(int)*12);
+  arr[0] = 1,arr[1] = 2,arr[2] = 3;
+
+  printf("%p:p1\n",p1);
+  printf("%p:p2\n",p2);
+  printf("%p:",str);
+  printf("%s\n",str);
+  printf("%p:",arr);
+  for(int i=0;i<12;i++)
+  {
+	  printf("%d ",arr[i]);
+  }
+  printf("\n");
+
+  mm_free(p2);
+  mm_free(str);
+  int* barr = mm_malloc(sizeof(int)*4);
+  barr[0] = 6,barr[1] = 7,barr[2] = 8;
+
+  arr = mm_realloc(arr,sizeof(int)*16);
+
+  printf("%p:",barr);
+  for(int i=0;i<4;i++)
+  {
+	  printf("%d ",barr[i]);
+  }
+  printf("\n");
+  printf("%p:",arr);
+  for(int i=0;i<16;i++)
+  {
+	  printf("%d ",arr[i]);
+  }
+  printf("\n"); 
+
+  long long* p3 = mm_malloc(sizeof(long long));
+  printf("%p:p3\n",p3);
+  int* p4 = mm_malloc(sizeof(int));
+  printf("%p:p4\n",p4);
+  barr = mm_realloc(arr,0);
+  mm_free(p1);
+  mm_free(barr);
+  mm_free(p3);
+  mm_free(p4);
+
+//  int* data = mm_malloc(sizeof(int));
+//  assert(data != NULL);
+//  data[0] = 0x162;
+//  mm_free(data);
+//  puts("malloc test successful!");
 }
